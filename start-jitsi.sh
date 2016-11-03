4e170d39b3bd4d0528f1eafcf878f00129c51611
joestriker <joe_striker@gmx.de>
2016-11-03 13:48:48 +0100

    Create start-jitsi.sh

---
+++ start-jitsi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARGS=""
+if [[ -n "$JITSI_SECRET" ]]; then
+    ARGS="$ARGS --secret=$JITSI_SECRET"
+fi
+
+if [[ -n "$JITSI_DOMAIN" ]]; then
+    ARGS="$ARGS --domain=$JITSI_DOMAIN"
+fi
+
+if [[ -n "$JITSI_HOST" ]]; then
+    ARGS="$ARGS --host=$JITSI_HOST"
+fi
+
+/usr/share/jitsi-videobridge/jvb.sh $ARGS
