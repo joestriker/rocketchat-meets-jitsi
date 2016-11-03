b8c84d062520c0d9c8ef99e43f7453af9dd3eda5
joestriker <joe_striker@gmx.de>
2016-11-03 13:50:06 +0100

    Update start-jitsi.sh

--- start-jitsi.sh
+++ start-jitsi.sh
@@ -2,15 +2,15 @@
 
 ARGS=""
 if [[ -n "$JITSI_SECRET" ]]; then
-    ARGS="$ARGS --secret=$JITSI_SECRET"
+	ARGS="$ARGS --secret=$JITSI_SECRET"
 fi
 
 if [[ -n "$JITSI_DOMAIN" ]]; then
-    ARGS="$ARGS --domain=$JITSI_DOMAIN"
+	ARGS="$ARGS --domain=$JITSI_DOMAIN"
 fi
 
 if [[ -n "$JITSI_HOST" ]]; then
-    ARGS="$ARGS --host=$JITSI_HOST"
+	ARGS="$ARGS --host=$JITSI_HOST"
 fi
 
 /usr/share/jitsi-videobridge/jvb.sh $ARGS
